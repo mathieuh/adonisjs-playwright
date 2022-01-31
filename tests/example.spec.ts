40db47ecd1bbfb711b70bcd13183e8bba71b0338
Mathieu Hordesseaux <mathieu.hordesseaux@gmail.com>
2022-01-31 14:20:57 +0100

    Add playwright as a library

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,12 @@
-import { test, expect } from '@playwright/test'
-import User from 'App/Models/User'
+import test from 'japa'
+// import User from 'App/Models/User'
+import { chromium } from 'playwright'
 
-test('basic test', async ({ page }) => {
-  await page.goto('/');
-  const input = await page.getAttribute('input', 'value')
-  expect(input).toBe('Virk')
+test('basic test', async (assert) => {
+  const browser = await chromium.launch({ headless: false, slowMo: 100 })
+  const page = await browser.newPage()
+  await page.goto('http://whatsmyuseragent.org/')
+  await page.pause()
+  // const input = await page.getAttribute('input', 'value')
+  // expect(input).toBe('Virk')
 })
